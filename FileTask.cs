ed2b8b8c2b34544ac795acee5508d4ec2fbf8b6b
Viktoriya Nikolaeva <v2.nikolaeva@a1qa.com>
2022-09-14 09:48:06 +0400

    compare array and files commit

---
+++ FileTask.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace C_Basics
+{
+    class FileTask
+    {
+        public static void SelectCases(string filePath, int numberOfRequiredCases = 10)
+        {
+            //read input file
+            var FileTxtLines = File.ReadAllLines(filePath, Encoding.UTF8);
+            if (FileTxtLines.Length - 1 < numberOfRequiredCases)
+            {
+                Console.WriteLine("The number of required cases is more than number cases in File.txt.");
+                return;
+            }
+            //cut required line numbers
+            var selectedCases = new List<string>();
+            var random = new Random();
+            var indices = new List<int>();
+            while (indices.Count < numberOfRequiredCases)
+            {
+                int index = random.Next(1, FileTxtLines.Length);
+                if (indices.Count == 0 || !indices.Contains(index))
+                {
+                    indices.Add(index);
+                    selectedCases.Add(FileTxtLines[index]);
+                }
+            }
+            //save input file without cut lines
+            var remainedLinesInFileTxt = FileTxtLines.Except(selectedCases).ToArray();
+            selectedCases.Insert(0, FileTxtLines[0]);
+            var extension = Path.GetExtension(filePath);
+            var resultFilePath = filePath.Replace(extension, $"_res{extension}");
+            File.WriteAllLines(filePath, remainedLinesInFileTxt);
+            //write cut lines to a new file
+            File.WriteAllLines(resultFilePath, selectedCases);
+            //return the path of the new file
+            Console.WriteLine($"New File path: {resultFilePath}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+
+namespace C_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < ArrayWay().Length; i++)
+                Console.WriteLine(ArrayWay()[i]);
+
+            foreach (string element in NoArrayWay())
+                Console.WriteLine(element);
+
+            string filePath = "C:\\Users\\v2.nikolaeva\\Visual Studio Projects\\C_Basics\\File.txt";
+            int numberOfCases = FileUtils.InsertNumberOfCases();
+            FileTask.SelectCases(filePath, numberOfCases);
+            Console.ReadLine();
+        }
+
+        public static string[] ArrayWay()
+        {
+            int counter = 0;
+            string[] array1 = { "Alex", "Dima", "Kate", "Galina", "Ivan" };
+            string[] array2 = { "Dima", "Ivan", "Kate" };
+            string[] resultArray = new string[100];
+            bool isEqualElemеntExist;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                isEqualElemеntExist = false;
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i].Equals(array2[j]))
+                    {
+                        isEqualElemеntExist = true;
+                    }
+                }
+
+                if (isEqualElemеntExist == false)
+                {
+                    counter++;
+                    resultArray[counter] = array1[i];
+                }
+            }
+            return resultArray;
+
+        }
+
+
+        public static IList NoArrayWay()
+        {
+            ArrayList list1 = new ArrayList() { "Alex", "Dima", "Kate", "Galina", "Ivan" };
+            ArrayList list2 = new ArrayList() { "Dima", "Ivan", "Kate" };
+
+            Boolean isEqualNameExist;
+            ArrayList resultList = new ArrayList();
+            foreach (string element in list1)
+            {
+                isEqualNameExist = false;
+                foreach (string element2 in list2)
+                {
+                    if (element.Equals(element2))
+                    {
+                        isEqualNameExist = true;
+                    }
+                }
+                if (isEqualNameExist == false)
+                {
+                    resultList.Add(element);
+                }
+            }
+            return resultList;
+        }
+    }
+}
+
